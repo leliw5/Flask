8902ab1627db6ccf43b1b73b1a2a8294c7400603
Leliw5 <lelon9@gmail.com>
2019-10-17 11:08:43 +0200

    Adding Thread module Put log_request function inside do_search

--- vsearch_for_web.py
+++ vsearch_for_web.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, escape, session
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from vsearch import search4letters
 from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
+from threading import Thread
 
 app = Flask(__name__)
 
@@ -28,30 +29,30 @@ def do_logout() -> str:
     return 'You are now logged out'
 
 
-def log_request(req: 'flask_request', res: str) -> None:
-    """Log details of the web request and the results."""
-
-    with UseDatabase(app.config['db_config']) as cursor:
-        _SQL = """insert into log  
-                  (phrase, letters, ip, browser_string, results) 
-                  values 
-                  (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                              req.form['letters'],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res,))
-
-
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
-    """Extract the posted data; perform the search; return results."""
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Log details of the web request and the results."""
+
+        with UseDatabase(app.config['db_config']) as cursor:
+            _SQL = """insert into log  
+                      (phrase, letters, ip, browser_string, results) 
+                      values 
+                      (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                  req.form['letters'],
+                                  req.remote_addr,
+                                  req.user_agent.browser,
+                                  res,))
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
     try:
-        log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as err:
         print('***** Logging failed this error:', str(err))
     return render_template('results.html',
